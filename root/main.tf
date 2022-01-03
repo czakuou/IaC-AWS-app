23595fd33b9af53c7f2fa7d8400a4b862e9addc3
Emil Krzaczyński <emil.krzakuou@gmail.com>
2022-01-03 11:07:55 +0100

    add instance and autoscaling balancer

--- root/main.tf
+++ root/main.tf
@@ -2,3 +2,12 @@ module "networking" {
   source    = "./modules/networking"
   namespace = var.namespace
 }
+
+module "autoscaling" {
+  source      = "./modules/autoscaling"
+  namespace   = var.namespace
+  ssh_keypair = var.ssh_keypair
+
+  vpc = module.networking.vpc
+  sg  = module.networking.sg
+}
